f8219459184785a04a970d09ac13868ac5285fc8
jthuko <johnthuko@gmail.com>
2023-10-18 21:50:44 -0500

    Added Binding Context for Radio Button

--- Converter/ChoiceToColorConverter.cs
+++ Converter/ChoiceToColorConverter.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MblexApp.Converter
+namespace MblexApp
 {
     class ChoiceToColorConverter:IValueConverter
     {
--- Converter/CorrectAnswerToColorConverter .cs
+++ Converter/CorrectAnswerToColorConverter .cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MblexApp.Converter
+namespace MblexApp
 {
     class CorrectAnswerToColorConverter:IValueConverter
     {
